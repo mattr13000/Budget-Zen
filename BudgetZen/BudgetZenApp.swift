969a56422655e381ee4671643eaa6a23c3a5c6cf
mattr13000 <mattjeux13@gmail.com>
2026-08-17 17:38:33 +0200

    Initial Commit

---
+++ BudgetZen/BudgetZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetZenApp.swift
+//  BudgetZen
+//
+//  Created by Apprenant 77 on 17/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
